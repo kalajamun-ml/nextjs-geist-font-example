55bf85b356bc5dea2c6e7d57a5af67579982078a
kalajamun-ml <techhubworld7@gmail.com>
2025-06-07 18:36:15 +0530

    Add .next/static/chunks/src_components_BikeDetail_tsx_0133d0ee._.js 1749301574619

---
+++ .next/static/chunks/src_components_BikeDetail_tsx_0133d0ee._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/BikeDetail.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/_cfd1111b._.js",
+  "static/chunks/src_components_BikeDetail_tsx_3ade99d7._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/BikeDetail.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
